2916c720e52fca5b1358c8f291ede13050cc9efd
Quentin Stevens <qusteven@ulb.ac.be>
2019-03-06 11:28:27 +0100

    beau template pour le site

--- website/leddyd/hello.py
+++ website/leddyd/hello.py
@@ -1,4 +1,7 @@
 import paho.mqtt.client as mqtt
+import paho.mqtt.subscribe as subscribe
+import json
+
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -6,12 +9,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template('home.html')
+    led_porte = receiveColor("porte")
+    led_radio = receiveColor("radioactif")
+    led_infra = receiveColor("infrabel")
+    return render_template('index.html', led_porte=led_porte, led_radio=led_radio, led_infra=led_infra)
 
 
 @app.route('/plafond')
 def my_form_plafond():
-    return render_template('multicolor.html')
+    return render_template('multicolor.html', hexa_col=receiveColor("plafond"), titre="Plafond")
 
 
 @app.route('/plafond', methods=['POST'])
@@ -23,7 +29,7 @@ def my_form_post_plafond():
         coul_b = int(hexa_coul[5:7], 16)
 
         sendColor(coul_r, coul_g, coul_b, "plafond")
-        return render_template('multicolor.html', hexa_col=hexa_coul)
+        return render_template('multicolor.html', hexa_col=receiveColor("plafond"), titre="Plafond")
     else:
         sendColor(0, 0, 0, "plafond")
         return render_template('multicolor.html')
@@ -31,47 +37,29 @@ def my_form_post_plafond():
 
 @app.route('/porte')
 def my_form_porte():
-    return render_template('on_off.html')
-
-
-@app.route('/porte', methods=['POST'])
-def my_form_post_porte():
-    led_on = request.form['switch']
-    if led_on == "on":
-        sendColor(255, 255, 255, "porte")
-    elif led_on == "off":
-        sendColor(0, 0, 0, "porte")
-    return render_template("on_off.html", led=True if led_on == "on" else False)
+    return render_template('on_off.html', titre="Porte")
 
 
 @app.route('/radioactif')
 def my_form_radioactif():
-    return render_template('on_off.html')
-
-
-@app.route('/radioactif', methods=['POST'])
-def my_form_post_radioactif():
-    led_on = request.form['switch']
-    if led_on == "on":
-        sendColor(255, 255, 255, "radioactif")
-    elif led_on == "off":
-        sendColor(0, 0, 0, "radioactif")
-    return render_template("on_off.html", led=True if led_on == "on" else False)
+    return render_template('on_off.html', titre="Radioactif")
 
 
 @app.route('/infrabel')
 def my_form_infrabel():
-    return render_template('on_off.html')
+    return render_template('on_off.html', titre="Infrabel")
 
 
-@app.route('/infrabel', methods=['POST'])
-def my_form_post_infrabel():
-    led_on = request.form['switch']
-    if led_on == "on":
-        sendColor(255, 255, 255, "infrabel")
-    elif led_on == "off":
-        sendColor(0, 0, 0, "infrabel")
-    return render_template("on_off.html", led=True if led_on == "on" else False)
+@app.route("/background_process", methods=['POST'])
+def backend():
+    led_state = request.form.get("on")
+    topic = json.loads(request.form.get("topic")).lower()
+    print(led_state)
+    if led_state == "true":
+        sendColor(255, 255, 255, topic)
+    else:
+        sendColor(0, 0, 0, topic)
+    return led_state
 
 
 def sendColor(r, g, b, topic):
@@ -79,6 +67,11 @@ def sendColor(r, g, b, topic):
     client.connect("hal.lan")
     client.publish("color/{}".format(topic), payload=bytes([r, g, b]), qos=1, retain=True)
 
+
+def receiveColor(topic):
+    messages = subscribe.simple("color/{}".format(topic), qos=1, retained=True, hostname="hal.lan")
+    color = "#" + messages.payload.hex()
+    return color
 
 
 if __name__ == '__main__':
