e05c3e3e377fd5b3abc912c6ed2c5d146680ac85
altf4arnold <arnold.dech@adct.be>
2019-03-18 18:31:15 +0100

    garder quand même la copie pour ne pas être bloqué si on ne peut pas importer

--- templates/README.md
+++ templates/README.md
@@ -1,6 +1,2 @@
 # Templates :
 Le fichier template.cpp contient une template que vous pouvez utiliser pour les wemos.
-
-Celle-ci permet d'importer les fichiers communs placés dans le dossier wemos
-et de lui permettre la connexion de base au WiFi, au MQTT et permetra par la suite
-les upgrades via le réseau.
--- templates/template.cpp
+++ templates/template.cpp
@@ -1,12 +1,55 @@
+#include "ESP8266WiFi.h"
+#include "PubSubClient.h"
 #include <ArduinoOTA.h>
-#include "../../template.cpp"
 
-char hostname = 'Le hostname a mettre';
+const char* hostname = "MY_HOSTNAE(folder_name)";
+const char* ssid = SSID;
+const char* password = PASSWORD;
+const char* mqtt_server = "hal.lan";
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
 void setup() {
-  working_setup(hostname);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    setup_wifi();
+    ArduinoOTA.begin();
+    mqttClient.setServer(mqtt_server, 1883);
+}
+
+void setup_wifi() {
+    delay(10);
+
+    String superHostname = String("hal-") + hostname;
+    WiFi.mode(WIFI_STA);
+    WiFi.hostname(superHostname);
+    WiFi.setAutoConnect(true);
+    WiFi.setAutoReconnect(true);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+}
+
+void reconnect() {
+    String message = String(hostname) + " " + WiFi.localIP().toString() + " " + WiFi.macAddress();
+    while (!mqttClient.connected()) {
+        if (mqttClient.connect("ESP8266Client")) {
+            mqttClient.publish("connect", message.c_str());
+        } else {
+            delay(5000);
+        }
+    }
 }
 
 void loop() {
-  working_loop(hostname);
+    if (!mqttClient.connected()) {
+        reconnect();
+    }
+    mqttClient.loop();
+    ArduinoOTA.handle();
 }
