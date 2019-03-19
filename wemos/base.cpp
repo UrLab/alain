1b30cce4d7d1387b7c24e006b880b66332f6dcc7
altf4arnold <arnold.dech@adct.be>
2019-03-19 16:03:30 +0100

    j'avais oublié de modifier une ligne après le rename

--- wemos/base.cpp
+++ wemos/base.cpp
@@ -2,7 +2,7 @@
 #include "PubSubClient.h"
 #include <ArduinoOTA.h>
 #include "wifiConfig.h"
-#include "template.hpp"
+#include "base.hpp"
 
 // char* hostname = "MY_HOSTNAE(folder_name)";
 const char* ssid = SSID;
