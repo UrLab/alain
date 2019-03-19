8ad8273ef68ad8d02d8c95d3e5eb0d0401fa84fe
altf4arnold <arnold.dech@adct.be>
2019-03-19 18:50:41 +0100

    Virer des trucs prototypes

--- wemos/base.hpp
+++
@@ -1,7 +0,0 @@
-void working_loop(char hostname);
-void reconnect(char hostname);
-void setup_wifi(char hostname);
-void working_setup(char hostname);
-extern const char *mqtt_server;
-extern const char *password;
-extern const char *ssid;
